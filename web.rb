9147df792dc123ec3bf544d225f332ddf018f6fd
Carlos Ramos <carakan@gmail.com>
2017-02-14 11:10:32 -0400

    change

--- web.rb
+++ web.rb
@@ -61,10 +61,3 @@ HEREDOC
   }.to_json
 end
 
-options "*" do
-  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
-
-  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-
-  200
-end
